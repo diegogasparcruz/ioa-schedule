9de55da2737c6f11fbda630e3334158cc6e3fe8a
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-07-08 12:36:47 -0300

    style: add tokens theme

---
+++ src/styles/tokens/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  background: '#F0F2F5',
+  red: '#E52E4D',
+  green: '#33CC95',
+  blue: '#5429CC',
+  'blue-light': '#6933FF',
+  'text-title': '#363F5F',
+  'text-body': '#969CB3',
+  shape: '#FFFFFF',
+  grayscale: {
+    background: 'rgba(0, 0, 0, 0.5)',
+  },
+};
