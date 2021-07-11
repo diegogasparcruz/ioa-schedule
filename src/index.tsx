b0e83c3ffdc6acdf936ab99f51e1bec258378da3
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-07-11 15:16:38 -0300

    refactor: remove miragejs config

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer, Model } from 'miragejs';
 import App from './App';
-
-createServer({
-  models: {
-    contact: Model,
-  },
-  seeds(server) {
-    server.db.loadData({
-      contacts: [
-        {
-          id: 1,
-          name: 'Hefanys Cavalcante',
-          email: 'hefanys@all.com',
-          phone: '(88) 98888-8888',
-        },
-        {
-          id: 2,
-          name: 'Diego Gaspar',
-          email: 'diego@all.com',
-          phone: '(88) 98888-8888',
-        },
-      ],
-    });
-  },
-  routes() {
-    this.namespace = 'api';
-
-    this.get('/contacts', () => this.schema.all('contact'));
-
-    this.post('/contacts', (schema, request) => {
-      const data = JSON.parse(request.requestBody);
-
-      return schema.create('contact', data);
-    });
-  },
-});
 
 ReactDOM.render(
   <React.StrictMode>
