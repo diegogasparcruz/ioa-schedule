5efba2ab5d47bee75b0d42a89799e302646f9062
Diego Gaspar <diegogaspardacruz1@gmail.com>
2021-07-13 00:45:40 -0300

    refactor: moves fetch all contacts

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,27 @@
+import { useEffect } from 'react';
 import { ContactsTable } from 'components/contextual/ContactsTable';
 import { Icon } from 'components/shared/core/Icon';
 import { Input } from 'components/shared/core/Input';
 import { useAppDispatch, useAppSelector } from 'hooks/store';
-import { useEffect } from 'react';
-import { fetchContacts } from 'store/contactSlice';
+import { api } from 'services/api';
+import { setContacts, setLoading } from 'store/contactSlice';
 
 import * as S from './Home.styles';
 
 export function Home() {
-  const { contacts, loading } = useAppSelector((state) => state.contact);
   const dispatch = useAppDispatch();
+  const { contacts, loading } = useAppSelector((state) => state.contact);
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(setLoading(true));
+    api
+      .get('/contacts')
+      .then((response) => {
+        dispatch(setContacts(response.data));
+      })
+      .finally(() => {
+        dispatch(setLoading(false));
+      });
   }, [dispatch]);
 
   return (
@@ -21,8 +30,8 @@ export function Home() {
         <Input placeholder="Digite um nome ou telefone">
           <Icon name="search" />
         </Input>
-        {loading && <span>Loading...</span>}
-        <ContactsTable contacts={contacts} />
+
+        <ContactsTable contacts={contacts} isLoading={loading} />
       </S.Main>
     </S.Container>
   );
